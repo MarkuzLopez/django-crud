fd41df626faf97215f3bd6610444e60034320917
MarkuzLopez <marco.02lopez@hotmail.com>
2025-04-24 12:37:32 -0600

    feat: configurate urls for request APIs of serializers

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskListCreateView, TaskRetrieveUpdateDeleteView
+from .views import TaskListCreateView, TaskRetrieveUpdateDeleteView, UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskRetrieveUpdateDeleteView.as_view(), name='task-detail'),
+    # step 4 configurate urls for request APIs of serializers 
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
 ]
