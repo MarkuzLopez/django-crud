391023ef8ff4790dae45c3184235abee495d0167
MarkuzLopez <marco.02lopez@hotmail.com>
2025-04-24 12:38:45 -0600

    feat: create request REST
    add request type:  create, get, update, delete with restframework import generics

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import generics
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, User
+from .serializers import TaskSerializer, UserSerializer
 # Create your views here.
 
 class TaskListCreateView(generics.ListCreateAPIView):
@@ -12,3 +12,18 @@ class TaskListCreateView(generics.ListCreateAPIView):
 class TaskRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+#step 3 create request REST: create, get, update, delete
+#with base to model in ths case fo users
+
+#class for obtain list and for create users
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    
+#class with that allows update, delete users for by ID 
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
