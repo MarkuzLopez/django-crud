a09ec43eaa51cb5e5a7ea87792af35efb9957164
MarkuzLopez <marco.02lopez@hotmail.com>
2025-04-24 12:34:30 -0600

    feat: creating  model for BD
    model of user in folder myapp/model

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator, MinLengthValidator
 
 # Create your models here.
 class Task(models.Model):
@@ -10,3 +11,28 @@ class Task(models.Model):
     def __str__(self):
         return self.title()
     
+# step create model for BD
+# after in cosole the migrations: 
+# python manage.py makemigrations and python manage.py migrate
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=200)
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+        help_text="Ingrese email valido",
+    )
+    phone_number = models.CharField(
+        max_length=15,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?\d{9,15}$',
+                message="el numerop debe tener entre 9 y 15 digitos",                
+            )
+        ],
+        verbose_name="Numero de Telefono"
+    )
+    
+    def __str__(self):
+         return f"{self.name} - {self.email}"
+         
